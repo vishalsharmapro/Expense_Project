8cf28e51c70ca9f42d4cc95bfaef8c85a258aabc
vishalsharmapro <vishalsharma.pro@gmail.com>
2026-03-13 20:21:19 +0530

    Added password reset feature and improved authentication UI

--- expense_project/settings.py
+++ expense_project/settings.py
@@ -119,3 +119,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'vishalsharma.pro@gmail.com'
+EMAIL_HOST_PASSWORD = 'dqms kbhk udhn pjyf'
--- expense_project/urls.py
+++ expense_project/urls.py
@@ -18,11 +18,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('expense.urls')),
     path('', include('accounts.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='accounts/password_reset.html'
+    ), name='password_reset'),
+
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'
+    ), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+
+    path('reset_done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
 
